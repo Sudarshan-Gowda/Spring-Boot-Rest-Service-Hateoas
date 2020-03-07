179f9332f89dbcc777df7e40f7f1dbc37cf72914
Sudarshan <sudarshan.java.p@gmail.com>
2020-03-07 13:06:17 +0800

    Base Release

---
+++ src/main/java/com/star/sud/employee/service/IEmployeeService.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.star.sud.employee.service;
+
+import java.util.List;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+
+import com.star.sud.employee.dto.Employee;
+
+/**
+ * @author sudarshan
+ *
+ */
+public interface IEmployeeService {
+
+	/**
+	 * @param employee
+	 * @return
+	 * @throws Exception
+	 */
+	ResponseEntity<Object> createEmployee(Employee employee);
+
+	/**
+	 * @param empId
+	 * @return
+	 */
+	EntityModel<Employee> getEmployeeById(int empId);
+
+	/**
+	 * @return
+	 */
+	List<Employee> retrieveAllEmployees();
+
+}
